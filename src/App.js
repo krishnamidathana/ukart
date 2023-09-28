b7f2563d5b933f03932fa245ed180d5cf8dff6a3
krishna <krishnakrrish66@gmail.com>
2023-09-28 16:19:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 import "./App.css";
@@ -6,14 +6,14 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" exact element={<Home />} />
 
         <Route path="/cart" exact element={<Cart />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

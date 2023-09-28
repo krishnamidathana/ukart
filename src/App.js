a0c63f722c47224a4eff92c028bc6a37fe04d6f8
krishna <krishnakrrish66@gmail.com>
2023-09-28 15:39:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+
+        <Route path="/cart" exact element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
